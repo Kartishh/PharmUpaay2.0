42366d3f4b2133a15e6582a46950d256408e2ce7
bhrigu <bhrigutayal053@gmail.com>
2024-08-17 19:24:52 +0530

    Deployed the ML model in app

--- PharmUpaay/app/src/main/java/com/project/pharmupaay/MainActivity.java
+++ PharmUpaay/app/src/main/java/com/project/pharmupaay/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import com.project.pharmupaay.ml.TfliteModelAnother;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -19,6 +20,9 @@ import android.widget.TextView;
 
 
 import com.project.pharmupaay.R;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -63,6 +67,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void classifyImage(Bitmap image){
+        try {
+            TfliteModelAnother model =  TfliteModelAnother.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1,256,256, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
+            for(int i = 0; i < imageSize; i ++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            TfliteModelAnother.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            String[] classes = {"Apple__Apple_scab", "Apple_Black_rot", "Apple_Cedar_apple_rust", "Apple_healthy", "Blueberry_healthy", "Cherry(including_sour)Powdery_mildew", "Cherry(including_sour)healthy", "Corn(maize)Cercospora_leaf_spot Gray_leaf_spot", "Corn(maize)Common_rust", "Corn_(maize)Northern_Leaf_Blight", "Corn(maize)healthy", "Grape_Black_rot", "Grape_Esca(Black_Measles)", "Grape__Leaf_blight(Isariopsis_Leaf_Spot)", "Grape__healthy", "Orange_Haunglongbing(Citrus_greening)", "Peach__Bacterial_spot", "Peach_healthy", "Pepper,_bell_Bacterial_spot", "Pepper,_bell_healthy", "Potato_Early_blight", "Potato_Late_blight", "Potato_healthy","Raspberry_healthy", "Soybean_healthy", "Squash_Powdery_mildew", "Strawberry_Leaf_scorch", "Strawberry_healthy", "Tomato_Bacterial_spot", "Tomato_Early_blight", "Tomato_Late_blight","Tomato_Leaf_Mold","Tomato_Septoria_leaf_spot","Tomato_Spider_mites Two-spotted_spider_mite","Tomato_Target_Spot","Tomato_Tomato_Yellow_Leaf_Curl_Virus","Tomato_Tomato_mosaic_virus","Tomato__healthy"};
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+
+            result.setText(classes[maxPos]);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(resultCode == RESULT_OK){
@@ -73,6 +126,7 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setImageBitmap(image);
 
                 image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
             }else{
                 Uri dat = data.getData();
                 Bitmap image = null;
@@ -84,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setImageBitmap(image);
 
                 image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
